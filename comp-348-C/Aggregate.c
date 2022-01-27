f79b9a9621b4823a878395543de7a17323b36924
mBeir96 <beiro1996@gmail.com>
2022-01-27 16:06:03 -0500

    include the c assignment

---
+++ comp-348-C/Aggregate.c
@@ -0,0 +1,127 @@
+#include "Aggregate.h"
+
+///In case the input array is NULL 
+
+int main()
+{
+    float arr[5];
+    //float arr[] = {1, 4, 5, 6, -1};
+    float m = min(&arr, 5);
+    printf("%f", m); // -1.000000
+    return 0;
+}
+
+float min(float* arr[], int size)
+{
+    //efface
+    int* yo = NULL;
+
+    if(!arr)
+    {
+        printf ("FATAL ERROR: "
+                 "array is NULL "
+                 "at %s, line %d.", __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");         
+
+        exit(0);         
+    }
+
+    if(size < 0)
+    {
+        printf ("FATAL ERROR: "
+                 "size is non-positive "
+                 "%d at %s, line %d.", size, __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");  
+        exit(0);          
+    }
+
+    float *small = &arr[0];
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] < small)
+        {
+            small = &arr[i];
+        }
+    }
+
+    return *small;
+}
+
+float max(float* arr[], int size)
+{
+    if(!arr)
+    {
+        printf ("FATAL ERROR: "
+                 "array is NULL "
+                 "at %s, line %d.", __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");  
+        exit(0);           
+    }
+
+    if(size < 0)
+    {
+        printf ("FATAL ERROR: "
+                 "size is non-positive "
+                 "%d at %s, line %d.", size, __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");  
+        exit(0);           
+    }
+
+    float *big = &arr[0];
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] > big)
+        {
+            big = &arr[i];
+        }
+    }
+
+    return *big;
+}
+
+float sum(float* arr[], int size)
+{
+    if(!arr)
+    {
+        printf ("FATAL ERROR: "
+                 "array is NULL "
+                 "at %s, line %d.", __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");  
+        exit(0);           
+    }
+
+    if(size < 0)
+    {
+        printf ("FATAL ERROR: "
+                 "size is non-positive "
+                 "%d at %s, line %d.", size, __FILE__, __LINE__);
+
+        printf("\nProgram is aborted");  
+        exit(0);           
+    }
+
+    float total;
+    for(int i = 0; i < size; i++)
+    {
+        total += *arr[i];
+    }
+
+    return total;
+}
+
+float avg(float* arr[], int size)
+{
+
+    return (sum(arr, size) / size);
+}
+
+float pseudo_avg(float* arr[], int size)
+{
+    return (min(arr, size) + max(arr, size)) / 2;
+}
+
---
+++ comp-348-C/Aggregate.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> //to exit the program
+
+
+float min(float* arr[], int size);
+
+float max(float* arr[], int size);
+
+float sum(float* arr[], int size);
+
+float avg(float* arr[], int size);
+
+float pseudo_avg(float* arr[], int size);
