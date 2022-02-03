5789f4f2fa81b779413473ee06da4f238899d0ac
mBeir96 <beiro1996@gmail.com>
2022-02-03 15:11:47 -0500

    its not perfect and it need to be modify Q6

--- comp-348-C/LinkedList.c
+++ comp-348-C/LinkedList.c
@@ -7,33 +7,34 @@ typedef char atom;            //atom is a char
 struct _listnode;   //non-defined structure _listnode
 
 typedef struct {  //structure of element 
-eltype type;      // either LIST or ATOM
-union {
-atom a;            //data 
-struct _listnode* l;  //list 
-};
+    eltype type;      // either LIST or ATOM
+    union {
+        atom a;            //data 
+        struct _listnode* l;  //list 
+    };
 } element;
 
 typedef struct _listnode {  //structure of list 
-element el;
-struct _listnode* next;
-} * list;
+    element el;
+    struct _listnode* next;
+}  *list;
 
-const element NIL = { .type=LIST, .l=NULL };
+const element NIL = { .type = LIST, .l = NULL };
 
-list head = NULL ;
-head = (list)malloc(sizeof(list)); 
+//it need to be inside of a scope
+// list head = NULL;
+// head = (list) malloc(sizeof(list)); 
 
 
 //function takes atom and returns element with that value
 element aasel(atom a){
-     element temp = {.type=ATOM, .a=a}; 
-     return temp;
+    element temp = {.type = ATOM, .a = a}; 
+    return temp;
 }; 
 
 //function takes list, sets element to list and returns element
 element lasel(list l){
-    element temp = {.type=LIST, .l=l};
+    element temp = {.type = LIST, .l = l};
     return temp; 
 };
 
@@ -43,20 +44,73 @@ element car(element e){
     if(e.type != LIST){
         return NIL;
     }
+    //not sure?
+    return e.type;
     
+};
+
+// that returns tail of the list, represented by e.
+// The tail of a list with one or zero element is NIL. The tail of an element that is not a list is also NIL.
+list cdr(element e)
+{
+
+   
+};
+
+list cddr(element e)
+{
+
+};
+
+//that creates a new list whose car and cdr are the element e and the list l.
+// The memory for the newly created list is to be allocated dynamically.
+list cons (element e, list l)
+{
+    list head = NULL;
+    head = (list) malloc(sizeof(list)); 
+
+    head.el = {.type = e.type, .l = l}
+
+    return head;
+}; 
+
+//that creates a new list whose elements are shallow copies of elements in l1 and l2, appended.
+list append(list l1, list l2)
+{
+    //not sure what they want
+    list l = NULL;
+    l = (list) malloc(sizeof(list)); 
+
+    l = l1;
+    l1 = l2;
+    l2 = l;
+
+    free(l);
+
+};
+
+//that frees all the memory previously allocated by the whole list (including all its elements and its inner lists)
+void lfreer(list l)
+{
+    //maybe need to do a for loop
+    free(l->el.a);
+    free(l->el.l);
+    free(l->el.type);
+    free(l->next->el);
+    free(l);
 
 
 };
-list cdr(element e){};
-list cddr(element e){};
-list cons (element e, list l){}; 
-list append(list l1, list l2){};
-void lfreer(list l){};
-void print(element e){};
+
+void print(element e)
+{
+
+};
 
 
 
 int main(){
 
-
+    list head = NULL;
+    head = (list) malloc(sizeof(list)); 
 }; 
